0b3837b145d8f7e84c615e5a69d12252947b1494
WhatAbout <71294722+MaxIvaniuk@users.noreply.github.com>
2024-10-25 20:56:44 +0300

    add revalidating stored cache data function

--- actions/posts.js
+++ actions/posts.js
@@ -5,6 +5,7 @@ import { redirect } from 'next/navigation';
 
 import { storePost, updatePostLikeStatus } from '@/lib/posts';
 import { uploadImage } from '@/lib/cloudinary';
+import { revalidatePath } from 'next/cache';
 
 export async function createPost(prevState, formData) {
   const title = formData.get('title');
@@ -52,4 +53,5 @@ export async function createPost(prevState, formData) {
 
 export async function togglePostsLikeStatus(postId) {
   updatePostLikeStatus(postId, 2)
+  revalidatePath('/', 'layout')
 }
